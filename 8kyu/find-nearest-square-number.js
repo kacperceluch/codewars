357ec02f4565eb41486d74ee17384a71994d51a5
Kacper <kacel0@op.pl>
2023-02-13 12:47:11 +0100

    first codewars task

---
+++ 8kyu/find-nearest-square-number.js
@@ -0,0 +1,15 @@
+// Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), of a positive integer n.
+
+// For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+
+// If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+
+// My solution
+
+function nearestSq(n){
+    const a = Math.sqrt(n);
+    const b = Math.round(a);
+    const c = b*b;
+    
+    return c;
+  }
