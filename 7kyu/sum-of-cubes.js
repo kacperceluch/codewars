c625ef1f57ad372a144a2d17b69a4171aef062e5
Kacper <kacperceluch@gmail.com>
2023-02-15 09:26:50 +0100

    Add level 7kyu and task Sum of cubes

---
+++ 7kyu/sum-of-cubes.js
@@ -0,0 +1,13 @@
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+
+// Assume that the input n will always be a positive integer.
+
+// My solution
+
+function sumCubes(n){
+  let x = 0;
+  for ( let i = 1; i < (n+1) ; i++) {
+     x += Math.pow( i, 3);
+  }
+  return x;
+}
